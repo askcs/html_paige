97454f3c1cd756fc96a70e1458f14031b41d1835
MichaelMa <mma@ask-cs.com>
2012-08-24 13:58:27 +0200

    new settings with timeout appservice

--- src/js/Paige_core/settings.js
+++ src/js/Paige_core/settings.js
@@ -2,5 +2,5 @@
 //session.appServices = "http://10.200.200.201:8888/ns_michael/";
 //session.appServices = "http://sven.ask-services.appspot.com/ns_michael/";
 
-session.appServices = "http://10.200.200.201:8888/ns_timeout/";
-//session.appServices = "http://sven.ask-services.appspot.com/ns_timeout/";
+//session.appServices = "http://10.200.200.201:8888/ns_timeout/";
+session.appServices = "http://sven.ask-services.appspot.com/ns_timeout/";
