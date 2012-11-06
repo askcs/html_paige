f745881d19b9925604c2ffd457ca81331bc708e5
MichaelMa <mma@ask-cs.com>
2012-11-06 15:56:26 +0100

    Async way to register timeout users .

--- src/js/Paige_core/admin_app.js
+++ src/js/Paige_core/admin_app.js
@@ -2,10 +2,10 @@
 
 /* App Module */
 
-angular.module('timeoutAdmin', []).
+angular.module('timeoutAdmin', ['timeoutAdminFilters']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/partners', {templateUrl: '/admin/partnerlist.html',   controller: PartnerListCtrl}).
-//      when('/partners/:partnerId', {templateUrl: 'admin/phone-detail.html', controller: PhoneDetailCtrl}).
+      when('/addTimeout', {templateUrl: '/admin/addTimeout.html', controller: addTimeoutCtrl}).
       otherwise({redirectTo: '/partners'});
 }]);
--- src/js/Paige_core/admin_controller.js
+++ src/js/Paige_core/admin_controller.js
@@ -1,32 +1,42 @@
 
 function PartnerListCtrl($scope, $http) {
-//  $http.get('phones/phones.json').success(function(data) {
-//    $scope.phones = data;
-//  });
-//
-//  $scope.orderProp = 'age';
     
-    $http({method: 'GET', url: admin_session.appServices + "operator/getAllAskatars", headers : { 'X-SESSION_ID' : admin_session.sessionKey} }).
-    success(function(data, status, headers, config) {
-      // this callback will be called asynchronously
-      // when the response is available
-        
-        $scope.error = "Success : "+ data +" status: "+ status + " headers : " + headers;
-    }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with status
-      // code outside of the <200, 400) range
-       $scope.error = "Error : "+ data +" status: "+ status + " headers : " + headers;
-    });
     
-//    $scope.partners = [
-//      {"personA": "mma",
-//       "personB": "xiaoyu",
-//       "contact": "contact"},
-//       {"personA": "mma1",
-//        "personB": "xiaoyu1",
-//        "contact": "contact1"},
-//    ];
+    var getPartners = function($scope,$http){
+        $http({method: 'GET', url: admin_session.appServices + "timeout/admin/tmlist", headers : { 'X-SESSION_ID' : admin_session.sessionKey} }).
+        success(function(data, status, headers, config) {
+          // this callback will be called asynchronously
+          // when the response is available
+            $scope.partners = data;
+//            $scope.error = "Success : "+ data +" status: "+ status + " headers : " + headers;
+        }).
+        error(function(data, status, headers, config) {
+          // called asynchronously if an error occurs
+          // or server returns response with status
+          // code outside of the <200, 400) range
+           $scope.error = "Error : "+ data +" status: "+ status + " headers : " + headers;
+        });
+    }
+    
+    getPartners($scope,$http);
+    
+    $scope.removeTimeout = function(personId) {
+        if(window.confirm("Are you sure you want to delete Timeout for user "+personId + "?")){
+            var para = {"userId" : personId};
+            dataCon.post("timeout/removeTimeout",para,function(res){
+                
+                if(res == "ok"){
+                    getPartners($scope,$http);
+                }else{
+//                    alert("no Timeout config for this user : "+personId);
+                    alert_timeout(res);
+                    getPartners($scope,$http);
+                }
+            });  
+        }
+    }
+}
+
+function addTimeoutCtrl($scope){
     
 }
