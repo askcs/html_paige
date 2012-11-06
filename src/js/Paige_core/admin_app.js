4b160291d0f165f2fc7e9d537d86a80c4f21aa7c
MichaelMa <mma@ask-cs.com>
2012-11-06 16:00:45 +0100

    remove a uncessary route.

--- src/js/Paige_core/admin_app.js
+++ src/js/Paige_core/admin_app.js
@@ -6,6 +6,6 @@ angular.module('timeoutAdmin', ['timeoutAdminFilters']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/partners', {templateUrl: '/admin/partnerlist.html',   controller: PartnerListCtrl}).
-      when('/addTimeout', {templateUrl: '/admin/addTimeout.html', controller: addTimeoutCtrl}).
+//      when('/addTimeout', {templateUrl: '/admin/partnerlist.html', controller: addTimeoutCtrl}).
       otherwise({redirectTo: '/partners'});
 }]);
